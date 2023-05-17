718fe51b21e177afe9413fa831087c273ead04d7
Hritik Choukikar <59253671+Hritik-choukikar@users.noreply.github.com>
2023-05-17 16:07:00 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,11 +5,27 @@ import streamlit as st
  
 uploaded_file = st.file_uploader("Upload Image")
   
+st.header("Spam mail Detector app")
+col1,col2=st.columns([2,2])
+
+col1.subheader("Enter the text")
+spam=col1.text_area(label='')
+
+model=load_model()
+
+k=st.button(label='Predict')
+if k:
+    predict=model.predict([spam])
+    print(predict[0])
+    if predict[0]==1:
+        st.write('Your Email is : Spam')
+    else:
+        st.write('Your Email is : Ham')
 
 
-# if __name__ == "__main__":
-#   uploaded_file = st.file_uploader("Upload Image")
-#   image = Image.open(uploaded_file)
- 
-#   st.image(image, caption='Input', use_column_width=True)
+
+st.text("")
+st.text("")
+st.write('Note: This Machine Learning model is trained on less amount of data, so the prediction of the model is not accurate')
+
 

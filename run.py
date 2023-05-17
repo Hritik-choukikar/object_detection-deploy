8475fed46e9ce7134dab4a1166f58528732b2961
Hritik Choukikar <59253671+Hritik-choukikar@users.noreply.github.com>
2023-05-17 16:00:05 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,14 @@
 import stream
 import streamlit as st
 
-
-if __name__ == "__main__":
-  uploaded_file = st.file_uploader("Upload Image")
-  image = Image.open(uploaded_file)
  
-  st.image(image, caption='Input', use_column_width=True)
+uploaded_file = st.file_uploader("Upload Image")
+  
+
+
+# if __name__ == "__main__":
+#   uploaded_file = st.file_uploader("Upload Image")
+#   image = Image.open(uploaded_file)
+ 
+#   st.image(image, caption='Input', use_column_width=True)
 

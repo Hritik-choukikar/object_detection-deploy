3ac140f1b2fc7a7796610d9df09a1946b3cf1e61
Hritik Choukikar <59253671+Hritik-choukikar@users.noreply.github.com>
2023-05-17 15:58:33 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
  
 import stream
+import streamlit as st
+
+
 if __name__ == "__main__":
-  k()
+  uploaded_file = st.file_uploader("Upload Image")
+  image = Image.open(uploaded_file)
+ 
+  st.image(image, caption='Input', use_column_width=True)
+
